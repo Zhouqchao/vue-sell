d376d3755e8b9742a30d08cee48aa345b4219033
zhouqichao <zhouqctech@gmail.com>
2018-03-15 13:06:26 +0800

    clear unnecessary warnings

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,7 @@
     created(){
       const url = 'https://gist.githubusercontent.com/Zhouqchao/64bfb02f304a0d6075c171285569e54d/raw/4590f3e79dccfc78caf740e1c1507426f7de84f0/vue-sell-data.json';
       this.$http.get(url + '?id=' + this.seller.id).then((res) => {
+        console.clear();
         res = res.body.seller;
         this.seller = Object.assign({},this.seller,res);
       });
